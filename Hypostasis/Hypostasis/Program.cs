d486b5c7ab570ed8cf89f3b67c447fa545f21eec
v.sumbaev <valdec4813@mail.ru>
2020-10-02 00:37:05 +0300

    Initial commit

---
+++ Hypostasis/Hypostasis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hypostasis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
